2eee9172a6fd699e8ae51237d3db0e7d56fd6953
Craig Hobbs <craigahobbs@gmail.com>
2021-11-24 13:46:48 -0800

    revert prior change

--- markdown-up/markdownUp.js
+++ markdown-up/markdownUp.js
@@ -39,7 +39,7 @@ struct MarkdownUp
     optional int(>= 1, <= 2) lineHeight
 
     # Variable collection
-    optional FieldValue{} variables
+    optional FieldValue{len > 0} variables
 
 
 # Field value union
