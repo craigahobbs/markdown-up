aaa38cc11d7ee2110f7c9ec7dc37ec3fac8c81d1
Craig Hobbs <craigahobbs@gmail.com>
2022-07-13 15:12:45 -0700

    add MarkdownScript library documentation

---
+++ calc-script/bin/calcScriptDoc.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+// Licensed under the MIT License
+// https://github.com/craigahobbs/calc-script/blob/main/LICENSE
+
+import {argv, exit, stderr, stdout} from '../node:process';
+import {parseLibraryDoc} from '../lib/libraryDoc.js';
+import {readFileSync} from '../node:fs';
+
+try {
+    stdout.write(JSON.stringify(parseLibraryDoc(argv.slice(2).map((file) => [file, readFileSync(file, 'utf-8')])), null, 4));
+} catch (error) {
+    stderr.write(error.message);
+    stderr.write('\n');
+    exit(1);
+}
