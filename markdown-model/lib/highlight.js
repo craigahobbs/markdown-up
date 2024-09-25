ba468c1870858826b31e630f569afa137f5ce376
craigahobbs@gmail.com <Craig A. Hobbs>
2024-09-25 16:47:34 -0700

    markdown-model 2.0.2

--- markdown-model/lib/highlight.js
+++ markdown-model/lib/highlight.js
@@ -225,16 +225,12 @@ const highlightLanguages = validateType(highlightTypes, 'HighlightMap', {
     // Shell
     'shell': {
         'builtin': [
-            'alias', 'bg', 'bind', 'break', 'builtin', 'caller', 'cd', 'command', 'compgen', 'complete', 'compopt',
-            'continue', 'declare', 'dirs', 'disown', 'echo', 'enable', 'eval', 'exec', 'exit', 'export', 'false', 'fc',
-            'fg', 'getopts', 'hash', 'help', 'history', 'jobs', 'kill', 'let', 'local', 'logout', 'mapfile', 'popd',
-            'printf', 'pushd', 'pwd', 'read', 'readarray', 'readonly', 'return', 'set', 'shift', 'shopt', 'source',
-            'suspend', 'test', 'times', 'trap', 'true', 'type', 'typeset', 'ulimit', 'umask', 'unalias', 'unset', 'wait'
+            'alias', 'declare', 'echo', 'exit', 'export', 'let', 'set', 'unset'
         ],
         'comment': [rCommentHash],
         'keyword': [
-            'case', 'do', 'done', 'elif', 'else', 'esac', 'fi', 'for', 'function', 'if', 'in', 'select', 'then', 'time',
-            'until', 'while'
+            'break', 'case', 'continue', 'do', 'done', 'elif', 'else', 'esac', 'fi', 'for', 'function', 'if', 'in',
+            'return', 'then', 'until', 'while'
         ],
         'string': [rStringSingle, rStringDouble]
     }
