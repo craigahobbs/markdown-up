2f10620ff0b1eda6c2019b065a106c3003a30751
Craig A. Hobbs <craigahobbs@gmail.com>
2026-04-23 14:01:23 -0700

    fix flaky tests

--- test/testApp.js
+++ test/testApp.js
@@ -354,12 +354,11 @@ function deleteElementCallbacks(elements) {
 
 // Drains the event loop (macrotasks + microtasks) so async click handlers have time to complete
 async function flushPromises() {
-    for (let ix = 0; ix < 3; ix++) {
+    for (let ix = 0; ix < 5; ix++) {
         // eslint-disable-next-line no-promise-executor-return
         await new Promise(resolve => setTimeout(resolve, 0));
         await Promise.resolve();
     }
-    await Promise.resolve();
 }
 
 
